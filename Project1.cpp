806b3dd15f94697dcd0dfe51b9b2ce70604d1e58
Musa Ahmed <machomusa1@gmail.com>
2022-10-05 15:56:03 -0700

    initial commit

--- Project1.cpp
+++ Project1.cpp
@@ -1,1 +1,18 @@
-Test
+// BlankConsoleLab.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+
+using namespace std;
+
+using std::cout;
+using std::cin;
+using std::endl;
+
+int main()
+{
+
+    cout << "Enter width: " << endl;
+    cout << "Enter length: ";
+}
+
